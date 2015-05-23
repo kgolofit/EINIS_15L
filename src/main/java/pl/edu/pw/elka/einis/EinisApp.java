9472519c34ae20e8439e48d604246d7e90684309
kgolofit <kamilg6@o2.pl>
2015-05-23 13:35:14 +0200

    Initialization of GUI

--- src/main/java/pl/edu/pw/elka/einis/EinisApp.java
+++ src/main/java/pl/edu/pw/elka/einis/EinisApp.java
@@ -1,10 +1,31 @@
 package pl.edu.pw.elka.einis;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
+ *
  */
-public class EinisApp {
+public class EinisApp extends Application
+{
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	launch(args); 	
     }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+	    
+        Scene scene = new Scene(root);
+    
+        stage.setTitle("EINIS App");
+        stage.setScene(scene);
+        stage.show();
+	}
 }
