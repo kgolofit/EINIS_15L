8ea9a5ab087c3e1fe6698eba8a6a8585b73344c8
siekiera <mistrzsiekiera@gmail.com>
2015-05-23 12:21:12 +0200

    Projekt Maven

---
+++ src/main/java/pl/edu/pw/elka/einis/EinisApp.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.elka.einis;
+
+/**
+ * Hello world!
+ *
+ */
+public class EinisApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
